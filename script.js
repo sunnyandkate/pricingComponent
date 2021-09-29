178c8504937ec725b190db9b0a455701c8387110
Kathrin Markworth <sunnyandkate@Kates-Air.fritz.box>
2021-09-29 11:07:43 +0100

    new commit

--- script.js
+++ script.js
@@ -6,24 +6,18 @@ let switchBtn = document.querySelector('.switch-btn');
 
 
 btn.addEventListener('click', function(){
-	
-	
+
 	if(btn.checked){
 		
 		price1.textContent= 199.99;
 		price2.textContent= 249.99;
 		price3.textContent= 399.99;
-	
 	}
 	else{
 		price1.textContent= 19.99;
 		price2.textContent= 24.99;
-		price3.textContent= 39.99;
-		
-	}
-	
-	
-	
+		price3.textContent= 39.99;		
+	}	
 });
 
 function keyCode(event) {
@@ -33,16 +27,12 @@ function keyCode(event) {
 		price2.textContent= 24.99;
 		price3.textContent= 39.99;
 		btn.checked =false;
-		
-	
 	}
-	else if(x == 37){
-		
+	else if(x == 37){	
 		price1.textContent= 199.99;
 		price2.textContent= 249.99;
 		price3.textContent= 399.99;
 		btn.checked = true;
 	}
-  
 }
 
